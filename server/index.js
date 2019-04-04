b806b1072703211abfa5cdb804f11648428b6263
Niyodusenga Jean Bosco <bosco7209@gmail.com>
2019-04-04 18:12:04 +0200

    [Feature #EndPoint] Creating initial app template

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT);
